5b0653b9e0130c3d6220b0706efaf7c2ffe5776a
Delanno Kotcho <delanktc24@gmail.com>
2024-08-02 12:46:33 +0200

    correction au niveau de la migration

--- database/migrations/2024_08_02_102249_create_groups_table.php
+++ database/migrations/2024_08_02_102249_create_groups_table.php
@@ -33,7 +33,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('group_user');
+        Schema::dropIfExists('groups');
         Schema::dropIfExists('groups');
     }
 };
