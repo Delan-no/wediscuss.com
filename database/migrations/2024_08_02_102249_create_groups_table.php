59a0969fafbf31f41423de379204e41d60c70875
Delanno Kotcho <delanktc24@gmail.com>
2024-08-02 12:48:35 +0200

    migration conversation créée

--- database/migrations/2024_08_02_102249_create_groups_table.php
+++ database/migrations/2024_08_02_102249_create_groups_table.php
@@ -33,7 +33,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('groups');
+        Schema::dropIfExists('group_user');
         Schema::dropIfExists('groups');
     }
 };
